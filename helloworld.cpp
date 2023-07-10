60be206f3658b54385b432beaa3612540cc6b646
NattiiZ <70894168+NattiiZ@users.noreply.github.com>
2023-07-10 10:39:33 +0700

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,81 @@
+/*#include <iostream>
+using namespace std;
+
+int main()
+{
+ cout << "Hello! My name's Nattawut Prasong \n";
+ cout << "ComPro Sec3 "<< endl;
+ cout << "Thank you! ";
+
+ system("pause");
+ return 0;
+}*/
+
+
+/*#include <iostream>
+
+int main()
+{
+ std::cout << "Student Code : 66-060216-3001-1 \n";
+ std::cout << "Student Name : Nattawut Prasong \n";
+ std::cout << "    Division : Infomation Thecnology \n";
+ std::cout << "        Year : 1 \n";
+ std::cout << "        Room : RC \n";
+ std::cout << "         Sex : Male \n";
+ std::cout << "         Age : 18 \n";
+
+ system("pause");
+ return 0;
+}*/
+
+
+#include <iostream>
+
+int main()
+{
+ std::cout << "Student Code : 66-060216-3001-1 \n";
+ std::cout << "Student Name : Nattawut Prasong \n";
+ std::cout << "    Division : Infomation Thecnology \n";
+ std::cout << "        Year : 1 \n";
+ std::cout << "        Room : RC \n";
+ std::cout << "         Sex : Male \n";
+ std::cout << "         Age : 18 \n";
+
+ system("pause");
+ return 0;
+}
+
+
+//------------------------------------------------
+
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+cout << "I love C++" << endl << "very much." << endl;
+return 0;
+}*/
+
+
+/*#include <iostream>
+void main()
+{
+cout << "Hello World.";
+}*/
+
+
+/*
+#include <iostream>
+void main()
+{
+std::cout << "Hello World.";
+}*/
+
+
+/*#include <iostream>
+using namespace std;
+void main()
+{
+cout << "Hello World.";
+}*/
