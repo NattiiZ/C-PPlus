83d26c8d7cec0747e754f6fbb8bf69c9ae2e0ea5
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-07-17 10:43:31 +0700

    Add files via upload

---
+++ BMI/BMI/bmi.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	float height , weight;
+
+	cout << "BMI CALCULATOR\n\n";
+	cout << "Enter your height (cm) : ";
+	cin >> height;
+	cout << "Enter your weight (kg) : ";
+	cin >> weight;
+
+	height = height / 100;
+	cout << "\nYour BMI = " << weight / pow(height , 2) << "\n\n";
+
+	system("pause");
+	return 0;
+}
