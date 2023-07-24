5fcd293b59f0c7c40ca7adfd1b145c048c7bd10a
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-07-24 10:11:33 +0700

    Add files via upload

---
+++ Score/Score/score.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;;
+
+int main()
+{
+	int score;
+
+	cout << "Enter Your C++ score : ";
+	cin >> score;
+
+	cout << "You " << ((score >= 50) ? "pass!" : "fail!") << " with score " << score << endl;
+	
+	/*
+	cout << "You ";
+	if (score >= 50)
+		cout << "pass";
+	else
+		cout << "fail";
+	cout << " with score " << score << endl;
+	*/
+	system("pause");
+	return 0;
+}
