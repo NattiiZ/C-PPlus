c7de47a071e26e7a51ec2deb875e1abcbd254949
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-07-30 18:02:03 +0700

    Add files via upload

---
+++ myInfo.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ cout << "Student Code : 66-060216-3001-1 \n";
+ cout << "Student Name : Nattawut Prasong \n";
+ cout << "    Division : Infomation Thecnology \n";
+ cout << "        Year : 1 \n";
+ cout << "        Room : RC \n";
+ cout << "         Sex : Male \n";
+ cout << "         Age : 18 \n";
+
+ system("pause");
+ return 0;
