d842647513ad700b3637151649eb68bd726339ab
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-07-30 16:38:47 +0700

    Add files via upload

---
+++ ASCII.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+/*int main()
+{
+	cout << 023 << endl;
+	cout << 23 << endl;
+	cout << 0x23 << endl;
+	system("pause");
+	return 0;
+}*/
+
+
+int main()
+{
+	cout << 230.E+3 << endl;
+	cout << 230E3 << endl;
+	cout << 230000.0 << endl;
+	cout << 2.3e5 << endl;
+	cout << 0.23E6 << endl;
+	cout << .23e+6 << endl;
+	system("pause");
+	return 0;
+}
