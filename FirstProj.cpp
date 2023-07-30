2341458e8490b7810ae8d9891e074f3363f39925
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-07-30 16:42:07 +0700

    Add files via upload

---
+++ FirstProj.cpp
@@ -0,0 +1,100 @@
+/*#include <iostream>
+using namespace std;
+
+int main()
+{
+ cout << "Hello! My name's Nattawut Prasong \n";
+ cout << "ComPro Sec3 "<< endl;
+ cout << "Thank you! ";
+
+ system("pause");
+ return 0;
+}*/
+
+
+/*#include <iostream>
+
+int main()
+{
+ std::cout << "Student Code : 66-060216-3001-1 \n";
+ std::cout << "Student Name : Nattawut Prasong \n";
+ std::cout << "    Division : Infomation Thecnology \n";
+ std::cout << "        Year : 1 \n";
+ std::cout << "        Room : RC \n";
+ std::cout << "         Sex : Male \n";
+ std::cout << "         Age : 18 \n";
+
+ system("pause");
+ return 0;
+}*/
+
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "\" \x4E\x41\x54 \"\n";
+	std::cout << "Size = " << sizeof(int) << " bytes\n";
+
+	system("pause");
+	return 0;
+}
+
+
+
+//---------------------TEST---------------------------
+
+
+/*#include <iostream>
+using namespace std;
+
+int main()
+{
+ cout << "I love C++\t very much. \\ \\ really \\ \\ \n";
+ cout << "A = \101 = \x41 ";
+
+ system("pause");
+ return 0;
+}*/
+
+
+
+/* Program 2_1 : Display with escape sequence */
+/*#include <iostream>
+using namespace std;
+int main()
+{
+cout << “12345678901234678901234567890\n”;
+cout << “ID : \t50-6626-120-1\n”;
+cout << “Name : \tSomchai Cheingpongpan\n”;
+cout << “\” Information Technology \”\n”;
+return (0);
+}*/
+
+
+
+/*#include <iostream>
+using namespace std;
+int main()
+{
+cout << "I love C++" << endl << "very much." << endl;
+return 0;
+}*/
+
+
+
+/*
+#include <iostream>
+void main()
+{
+std::cout << "Hello World.";
+}*/
+
+
+
+/*#include <iostream>
+using namespace std;
+void main()
+{
+cout << "Hello World.";
+}*/
