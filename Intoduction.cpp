ca51f470fa2b98a07c6193c896b385427aa3b37d
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-08-06 17:01:54 +0700

    Add files via upload

---
+++ Intoduction.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    cout << "I love C++\t very much. \\ \\ really \\ \\ \n";
+ 	cout << "A = \101 = \x41 ";
+
+	cout << "\n";
+	
+	cout << "\" \x4E\x41\x54 \"\n";
+	cout << "Size = " << sizeof(int) << " bytes\n";
+
+	cout << "\n";
+
+    cout << 230.E+3 << endl;
+    cout << 230E3 << endl;
+    cout << 230000.0 << endl;
+    cout << 2.3e5 << endl;
+    cout << 0.23E6 << endl;
+    cout << .23e+6 << endl;
+
+    cout << "\n";
+
+    system("pause");
+    return 0;
+}
