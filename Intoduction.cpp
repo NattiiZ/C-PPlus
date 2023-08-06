9812a18b5506c44df8e417c86dd558e6f0713167
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-08-06 17:03:19 +0700

    Update Intoduction.cpp

--- Intoduction.cpp
+++ Intoduction.cpp
@@ -13,15 +13,6 @@ int main()
 
 	cout << "\n";
 
-    cout << 230.E+3 << endl;
-    cout << 230E3 << endl;
-    cout << 230000.0 << endl;
-    cout << 2.3e5 << endl;
-    cout << 0.23E6 << endl;
-    cout << .23e+6 << endl;
-
-    cout << "\n";
-
     system("pause");
     return 0;
 }
