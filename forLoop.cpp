85428ae68065400cda800cf13ea0654ecec88bc0
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-08-07 10:13:05 +0700

    Add files via upload

---
+++ forLoop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(int n = 100; n >= 1; n=n-5)
+        cout << n << " ";
+
+    
+    cout << endl;
+
+    system("pause");
+    return 0;
+}
