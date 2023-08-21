81910a29b99cd0a568dc6aa1263909c4dd50b378
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-08-21 11:43:51 +0700

    Add files via upload

---
+++ ExamTest.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+    int random;
+    float total=0, score, Max=0, Sum, Avg;
+
+
+    srand(time(NULL));
+    random = 1+rand()%10;
+    cout << "Random number of student (1-10) : " << random << endl;
+    for (int i=1; i<=random; i++)
+    {
+        cout << "Input score " << i << " : ";
+        cin >> score;
+        if (score > Max)
+            Max = score;
+        total += score;
+    }
+    cout << setfill('-') << setw(35) << " " << endl;
+
+    cout << fixed << setprecision(2) << endl;
+    cout << "Max score\t= " << Max << endl;
+    cout << "Sum score\t= " << total << endl;
+    cout << "Avg. score\t= " << total/random << endl;
+
+    cout << setfill('-') << setw(35) << " " << endl;
+
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
