cee655a0e75fe1bdc234ef611983b3a00254041c
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-07 20:54:18 +0700

    Add files via upload

---
+++ function_strClass.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S = "Microsoft Visual Studio.Net";
+
+
+    cout << S.length() << endl;
+    cout << S.size() << endl;
+    cout << S.c_str() << endl;
+    cout << S.at(15) << endl;
+    cout << S.append(" 2010");
+
+    for (int i=0; i<S.length(); i++)
+        cout << S.at(i) << "-";
+
+    cout << endl;
+    cout << S.substr(10,6) << endl;
+    cout << S.find('s') << endl;
+    S.replace(10,6,"C++");
+    cout << S;
+
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
