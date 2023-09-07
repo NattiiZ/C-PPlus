e1b991cd5f429e97663df549b6d025b5ef71e659
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-07 20:57:15 +0700

    Add files via upload

---
+++ variableScope.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int G=20; // ----- Global
+
+int main()
+{
+    int a=10, b=20;  // ----- Local
+    int G=10;
+
+    cout << G << endl;
+
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
