667c8999096c112fab6e5fd497be82762ce7216d
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-08 12:49:05 +0700

    Add files via upload

---
+++ firstProj.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello World." << endl;
+
+	cout << "\n";
+
+	cout << "I love C++" << endl << "very much." << endl;
+
+	cout << "\n";
+
+	cout << "Hello! My name's Nattawut Prasong" << endl;
+	cout << "ComPro Sec 3"<< endl;
+	cout << "Thank you!" << endl;
+
+	cout << "\n";	
+
+	system("pause");
+	return 0;
+}
