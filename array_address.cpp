a6ec58e2a4d6b9ba6279a4ebdf23e5fedfa12f7b
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-18 21:24:07 +0700

    Add files via upload

---
+++ array_address.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num[10], b=10;
+
+    cout << num << endl;
+    cout << b << endl;
+    cout << &b << endl;
+
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
