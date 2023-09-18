8e8b0ab0734081d5d4dcca1fb57654a930964bae
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-18 21:26:14 +0700

    Add files via upload

---
+++ array_sizeof.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num[10];
+
+    cout << "Size = " << sizeof(num[5]) << endl;
+
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
