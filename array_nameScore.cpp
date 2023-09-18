cb743942519fb6757769d25e2db913db7051f334
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-18 11:20:39 +0700

    Add files via upload

---
+++ array_nameScore.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int score[4], total=0, maxScore=0, minScore=0;
+    string name[4];
+
+    for(int i=0; i<4; i++)
+    {
+        cout << "Enter Name [" << i+1 << "] : ";
+        cin >> name[i];
+        cout << "Enter Score [" << i+1 << "] : ";
+        cin >> score[i];
+        total += score[i];
+
+        if (score[i]>maxScore)
+            maxScore = score[i];
+        
+        minScore = score[0];
+        if (score[i]<minScore)
+            minScore=score[i];
+
+        cout << endl;
+    }
+
+    cout << "-" << setfill('-') << setw(22) << " " << endl;
+    cout << " Name\t\tScore" << endl;
+    cout << "-" << setfill('-') << setw(22) << " " << endl;
+    
+    for(int i=0; i<4; i++)
+        cout << " " << name[i] << "\t\t  " << score[i] << endl;
+    
+    cout << "-" << setfill('-') << setw(22) << " " << endl;
+
+    cout << "Max Score = " << maxScore << endl;
+    cout << "Min Score = " << minScore << endl;
+    cout << "Total Score = " << total << endl;
+    cout << "Average Score = " << float(total)/4 << endl;
+
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
