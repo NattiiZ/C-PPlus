a8874e9843dd74672b1ce20237624504db2730b6
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-18 21:25:07 +0700

    Update array_nameScore.cpp

--- array_nameScore.cpp
+++ array_nameScore.cpp
@@ -5,24 +5,16 @@ using namespace std;
 
 int main()
 {
-    int score[4], total=0, maxScore=0, minScore=0;
+    int score[4], total=0, maxScore=0, minScore;
     string name[4];
 
-    for(int i=0; i<4; i++)
+     for(int i=0; i<4; i++)
     {
         cout << "Enter Name [" << i+1 << "] : ";
         cin >> name[i];
         cout << "Enter Score [" << i+1 << "] : ";
         cin >> score[i];
         total += score[i];
-
-        if (score[i]>maxScore)
-            maxScore = score[i];
-        
-        minScore = score[0];
-        if (score[i]<minScore)
-            minScore=score[i];
-
         cout << endl;
     }
 
@@ -30,11 +22,19 @@ int main()
     cout << " Name\t\tScore" << endl;
     cout << "-" << setfill('-') << setw(22) << " " << endl;
     
+    minScore = score[0];
     for(int i=0; i<4; i++)
+    {
+        if (score[i]>maxScore)
+            maxScore = score[i];
+        
+        if (score[i]<minScore)
+            minScore=score[i];
+
         cout << " " << name[i] << "\t\t  " << score[i] << endl;
+    }
     
     cout << "-" << setfill('-') << setw(22) << " " << endl;
-
     cout << "Max Score = " << maxScore << endl;
     cout << "Min Score = " << minScore << endl;
     cout << "Total Score = " << total << endl;
