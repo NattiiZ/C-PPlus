e1ad9c64af89d27b928c9bdc48b349dc16f29e95
Nattawut Prasong <70894168+NattiiZ@users.noreply.github.com>
2023-09-23 02:56:31 +0700

    Add files via upload

---
+++ histogram.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <time.h>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int value[10], random;
+
+
+    srand(time(NULL));
+    for (int i=0; i<10; i++)
+    {
+        value[i] = 1+rand()%29;
+        cout << i << setfill(' ') << setw(4) << right << value[i] << " " << setfill('*') << setw(value[i]+1) << " " << endl;
+    }
+
+
+    cout << endl;
+    system("pause");
+    return 0;
+}
